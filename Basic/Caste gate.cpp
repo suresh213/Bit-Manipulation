1322eb3c09b3382e2e67c0493624ee67ce723b36
Suresh <61372017+suresh213@users.noreply.github.com>
2020-07-17 20:24:31 +0530

    Add files via upload

---
+++ Basic/Caste gate.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n,count=0;
+		cin>>n;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=i+1;j<=n;j++)
+			{
+				if((i^j)<=n)
+				{
+					count++;
+				}
+			}
+		}
+		cout<<count<<endl;
+	}	
+
+}
+
